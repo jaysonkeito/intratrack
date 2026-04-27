bcf2f1aa721a7c587b645c8083a7acddccbac4d0
Jayson P. Francisco <jayson.f.bsinfotech@gmail.com>
2026-04-27 10:30:27 +0800

    Changes in settings.py

--- intratrack/settings.py
+++ intratrack/settings.py
@@ -13,7 +13,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'jaysonkeito.pythonanywhere.com',
+    'intratrack.pythonanywhere.com',
     '*',  # remove in strict production
 ]
 
